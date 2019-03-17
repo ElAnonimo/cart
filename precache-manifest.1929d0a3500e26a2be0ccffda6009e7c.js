6f9dc05ed50211b9cef4b47d412c98204c89e885
elanonimo <mgrbmk@emailhosting.com>
2019-03-17 19:16:00 +0300

    Updates

---
+++ precache-manifest.1929d0a3500e26a2be0ccffda6009e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d876e8e1ea77f5dd07",
+    "url": "/cart2/static/js/runtime~main.d37c87d2.js"
+  },
+  {
+    "revision": "bcbb5179fb0f6a420f20",
+    "url": "/cart2/static/js/main.445b58d3.chunk.js"
+  },
+  {
+    "revision": "e1899ea7f12f9cdf953d",
+    "url": "/cart2/static/js/2.eeb6e0ef.chunk.js"
+  },
+  {
+    "revision": "bcbb5179fb0f6a420f20",
+    "url": "/cart2/static/css/main.09f81bbe.chunk.css"
+  },
+  {
+    "revision": "e1899ea7f12f9cdf953d",
+    "url": "/cart2/static/css/2.206a2283.chunk.css"
+  },
+  {
+    "revision": "a6b64cdb5e1fcfb54a00fc382d3b7d68",
+    "url": "/cart2/index.html"
+  }
+];
